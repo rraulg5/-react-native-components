f179802b9e57546666984c925f0e9da12177ecb4
Raul Garcia <rraulg5@gmail.com>
2021-08-20 14:59:41 -0500

    FlatList

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Icon name="star-outline" size={50} color="black" />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 };
